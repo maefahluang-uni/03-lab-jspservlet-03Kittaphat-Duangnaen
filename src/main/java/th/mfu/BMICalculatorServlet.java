9e7659b7ce8d05672569eb32f810a56fd0938f95
03Kittaphat-Duangnaen <122779827+03Kittaphat-Duangnaen@users.noreply.github.com>
2023-09-04 09:29:13 +0000

    6531503003

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -8,21 +8,41 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+@WebServlet("/calbmi")
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get parameters from the request: "weight" and "height"
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
+        // Calculate BMI
+        double bmi = weight / ((height) * (height));
+        int roundedBMI = (int) Math.round(bmi);
 
-        //TODO: forward to jsp
-           
+        // Determine the body type from BMI
+        String builtType = "normal";
+        if (bmi < 18.5) {
+            builtType = "underweight";
+        } else if (bmi < 25) {
+            builtType = "normal";
+        } else if (bmi < 30) {
+            builtType = "overweight";
+        } else if (bmi < 35) {
+            builtType = "obese";
+        } else {
+            builtType = "extremely obese";
+        }
+
+        // Add BMI and body type to the request's attributes
+        request.setAttribute("bmi", roundedBMI);
+        request.setAttribute("builtType", builtType);
+
+        // Forward to the JSP
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+
     }
-    
+
 }
